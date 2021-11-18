66e23e860da110aeb543754317e76a0e69534053
Charles Barnes <onthisrock97@gmail.com>
2021-11-18 13:17:49 -0500

    This commit marks the beginning of my time on Codeacademy revisiting what I have learned about C++.

---
+++ CodeAcademyC++Lesson0.cpp
@@ -0,0 +1,50 @@
+CodeAcademyC++Lesson0.cpp
+// Part 1 Hello World!
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Hello World!\n";
+
+}
+//Part 2 Output
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Codecademy is super cool!\n";
+  
+}
+//Part 3 Pattern
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "       1\n";
+  std::cout << "     2 3\n";
+  std::cout << "   4 5 6\n";
+  std::cout << "7 8 9 10\n";
+
+  
+}
+// Part 4 Review
+#include <iostream>
+
+int main() 
+{
+  
+std::cout << "Dear Self,\n";
+std::cout << "  My goals for you are as follows: to keep you strong,\n" ;std::cout << "competent, and happy. These will take the form of retu-\n";
+std::cout << "rning lifting 3x a week, studying the necessary langua-\n";
+std::cout << "ges and textbooks to pass the FE/GSEC/eJPT/and CompTIA\n";
+std::cout << "exams, attending my specific extracurricular meetings\n";
+std::cout << "and spending quality time with my family and girlfriend.\n";
+std::cout << "I know these are lofty goals but I am confident that\n";
+std::cout << "if I work diligently that you will be in a better place\n";
+std::cout << "in all areas of life. PERDURABO\n";
+std::cout << "Sincerely, \n";
+std::cout << "Charles Barnes \n";
+std::cout << "11/18/2021";
+  
+}
